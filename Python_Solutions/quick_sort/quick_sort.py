721f61b15062456153ba69f3430272068a799221
WreetSarker <wreet.sarker@gmail.com>
2022-01-08 09:00:58 +0600

    quick sort python implementation added

--- Python_Solutions/quick_sort/quick_sort.py
+++ Python_Solutions/quick_sort/quick_sort.py
@@ -1,1 +1,30 @@
 # Implement a quick sort algorithm
+
+def quick_sort(arr, left, right):
+    if left < right:
+        pivotIdx = partition(arr, left, right)
+        quick_sort(arr, left, pivotIdx - 1)
+        quick_sort(arr, pivotIdx + 1, right)
+    return arr
+
+
+def partition(arr, start, end):
+    pivot = arr[start]
+    currentIdx = start
+
+    for i in range(start, end+1):
+        if arr[i] < pivot:
+            currentIdx += 1
+            temp = arr[i]
+            arr[i] = arr[currentIdx]
+            arr[currentIdx] = temp
+    temp = arr[currentIdx]
+    arr[currentIdx] = arr[start]
+    arr[start] = temp
+    return currentIdx
+
+
+data = [1, 2, 51, 24, 56, 32, 12, 4,  7, 100, 9, -
+        1, -12, 21, 30, 34, -3, -21, 31, 34, -200, 300, 13, -220, 14, -240, 400]
+size = len(data)-1
+print(quick_sort(data, 0, size))

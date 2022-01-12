e4948ae87ac077389b13972e8373a2d785ca96d0
WreetSarker <wreet.sarker@gmail.com>
2022-01-12 12:48:06 +0600

    bst js solution added

---
+++ Js_Solutions/binary_search_tree/binary_search_tree.js
@@ -0,0 +1,74 @@
+// Implement a binary search tree
+
+class Node {
+    constructor(val) {
+        this.value = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(val) {
+        const newNode = new Node(val);
+        if (!this.root) {
+            this.root = newNode;
+            return true;
+        } else {
+            let current = this.root;
+            while (true) {
+                if (val === current.value) return false;
+                if (val > current.value) {
+                    if (current.right === null) {
+                        current.right = newNode;
+                        return true;
+                    } else {
+                        current = current.right;
+                    }
+                } else if (val < current.value) {
+                    if (current.left === null) {
+                        current.left = newNode;
+                        return true;
+                    } else {
+                        current = current.left;
+                    }
+                }
+            }
+        }
+    }
+
+    find(val) {
+        if (!this.root) {
+            return false;
+        } else {
+            let current = this.root;
+            let found = false;
+            while (current && !found) {
+                // if (val === current.value) return true;
+                if (val > current.value) {
+                    current = current.right;
+                } else if (val < current.value) {
+                    current = current.left;
+                } else {
+                    found = true;
+                }
+            }
+            return found;
+        }
+    }
+}
+
+tree = new BinarySearchTree();
+tree.insert(10);
+// console.log(tree);
+tree.insert(1);
+// console.log(tree);
+tree.insert(13);
+// console.log(tree);
+tree.insert(12);
+// console.log(tree);
+console.log(tree.find(13));

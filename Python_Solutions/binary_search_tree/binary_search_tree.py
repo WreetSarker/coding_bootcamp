acc3e182c6dc40b0dede08deb744d153805e5c9a
WreetSarker <wreet.sarker@gmail.com>
2022-01-13 11:12:02 +0600

    bst python solution added

--- Python_Solutions/binary_search_tree/binary_search_tree.py
+++ Python_Solutions/binary_search_tree/binary_search_tree.py
@@ -1,1 +1,59 @@
 # Implement a binary search tree
+
+class Node:
+    def __init__(self, val):
+        self.value = val
+        self.left = None
+        self.right = None
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, val):
+        newNode = Node(val)
+        if self.root is None:
+            self.root = newNode
+        else:
+            current = self.root
+            while True:
+                if current.value == val:
+                    return False
+                if val > current.value:
+                    if current.right is None:
+                        current.right = newNode
+                        return True
+                    else:
+                        current = current.right
+                elif val < current.value:
+                    if current.left is None:
+                        current.left = newNode
+                        return True
+                    else:
+                        current = current.left
+
+    def found(self, val):
+        if self.root is None:
+            return False
+        current = self.root
+        found = False
+
+        while (current is not None) and not found:
+            if val > current.value:
+                current = current.right
+            elif val < current.value:
+                current = current.left
+            else:
+                found = True
+        return found
+
+
+tree = BinarySearchTree()
+tree.insert(10)
+tree.insert(4)
+tree.insert(14)
+tree.insert(3)
+tree.insert(1)
+tree.insert(13)
+print(tree.found(2))

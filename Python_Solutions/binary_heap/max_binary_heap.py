85fc1c5ab0fdbc230ab2459de762ffec75f63bbf
WreetSarker <wreet.sarker@gmail.com>
2022-01-17 09:28:54 +0600

    binary heap python extract max solution added

--- Python_Solutions/binary_heap/max_binary_heap.py
+++ Python_Solutions/binary_heap/max_binary_heap.py
@@ -21,13 +21,55 @@ class MaxBinaryHeap:
             self.values[idx] = parent
             idx = parentIdx
 
+    def extract_max(self):
+        max_val = self.values[0]
+        end = self.values.pop()
+        if len(self.values) > 0:
+            self.values[0] = end
+            self.sink_down()
+        return max_val
+
+    def sink_down(self):
+        idx = 0
+        element = self.values[0]
+        length = len(self.values)
+
+        while True:
+            left_child_idx = 2*idx + 1
+            right_child_idx = 2*idx + 2
+            left_child = None
+            right_child = None
+            swap = None
+
+            if left_child_idx < length:
+                left_child = self.values[left_child_idx]
+                if left_child > element:
+                    swap = left_child_idx
+
+            if right_child_idx < length:
+                right_child = self.values[right_child_idx]
+                if (swap is None and right_child_idx > element) or (swap is not None and right_child > left_child):
+                    swap = right_child_idx
+
+            if swap is None:
+                break
+            self.values[idx] = self.values[swap]
+            self.values[swap] = element
+            idx = swap
+
 
 heap = MaxBinaryHeap()
-heap.insert(12)
-heap.insert(27)
-heap.insert(18)
-heap.insert(55)
-heap.insert(33)
 heap.insert(41)
-print(heap.insert(39))
+heap.insert(39)
+heap.insert(33)
+heap.insert(18)
+heap.insert(27)
+heap.insert(12)
+print(heap.insert(55))
 print(heap.insert(1000))
+print(heap.extract_max())
+print(heap.values)
+print(heap.extract_max())
+print(heap.values)
+heap.extract_max()
+print(heap.values)

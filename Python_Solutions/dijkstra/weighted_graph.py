6affd14f2faab6408c5cc236a2665c44345e292e
WreetSarker <wreet.sarker@gmail.com>
2022-02-04 08:23:55 +0600

    python naive pq added

--- Python_Solutions/dijkstra/weighted_graph.py
+++ Python_Solutions/dijkstra/weighted_graph.py
@@ -1,5 +1,20 @@
 # Implement Dijkstra's shortest path algorithm
 
+class PriorityQueue:
+    def __init__(self):
+        self.values = []
+
+    def enqueue(self, val, priority):
+        self.values.append({'value': val, 'priority': priority})
+        self.sort()
+
+    def dequeue(self):
+        return self.values.pop(0)
+
+    def sort(self):
+        self.values = sorted(self.values, key=lambda node: node['priority'])
+
+
 class WeightedGraph:
     def __init__(self):
         self.adjacencyList = {}
@@ -20,3 +35,9 @@ g.addVertex('C')
 g.addEdge('A', 'B', 15)
 g.addEdge('A', 'C', 25)
 print(g.adjacencyList)
+
+q = PriorityQueue()
+q.enqueue('A', 15)
+q.enqueue('B', 12)
+q.enqueue('C', 30)
+print(q.values)

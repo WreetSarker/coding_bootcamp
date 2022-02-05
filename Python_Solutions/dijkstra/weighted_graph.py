2042a2eb707cd4fb8d1fdba7fb468d70e26d221e
WreetSarker <wreet.sarker@gmail.com>
2022-02-05 07:58:58 +0600

    python naive dijkstra completed

--- Python_Solutions/dijkstra/weighted_graph.py
+++ Python_Solutions/dijkstra/weighted_graph.py
@@ -27,17 +27,60 @@ class WeightedGraph:
         self.adjacencyList[vertex1].append({'node': vertex2, 'weight': weight})
         self.adjacencyList[vertex2].append({'node': vertex1, 'weight': weight})
 
+    def Dijkstra(self, start, finish):
+        distances = {}
+        previous = {}
+        nodes = PriorityQueue()
+        path = []  # To return at the end
+        smallest = None
+
+        # Setting up initial values
+        for vertex in self.adjacencyList.keys():
+            if vertex == start:
+                distances[vertex] = 0
+                nodes.enqueue(vertex, 0)
+            else:
+                distances[vertex] = float('inf')
+                nodes.enqueue(vertex, float('inf'))
+            previous[vertex] = None
+
+        # Loop until there is something to visit
+        while len(nodes.values) > 0:
+            smallest = nodes.dequeue()['value']
+            if smallest == finish:
+                while previous[smallest] is not None:
+                    path.append(smallest)
+                    smallest = previous[smallest]
+                break
+            if (smallest is not None) or distances[smallest] != float('inf'):
+                for neighbor in self.adjacencyList[smallest]:
+                    # Calculate new distance to neighbor node
+                    candidate = distances[smallest] + neighbor['weight']
+                    if candidate < distances[neighbor['node']]:
+                        # update new distance to neighbor node
+                        distances[neighbor['node']] = candidate
+                        # update previous - How we got to neighbor
+                        previous[neighbor['node']] = smallest
+                        # Enqueue in priority queue with new priority
+                        nodes.enqueue(neighbor['node'], candidate)
+
+        path.append(smallest)
+        return list(reversed(path))
+
 
 g = WeightedGraph()
 g.addVertex('A')
 g.addVertex('B')
 g.addVertex('C')
-g.addEdge('A', 'B', 15)
-g.addEdge('A', 'C', 25)
-print(g.adjacencyList)
-
-q = PriorityQueue()
-q.enqueue('A', 15)
-q.enqueue('B', 12)
-q.enqueue('C', 30)
-print(q.values)
+g.addVertex('D')
+g.addVertex('E')
+g.addVertex('F')
+g.addEdge('A', 'B', 4)
+g.addEdge('A', 'C', 2)
+g.addEdge('B', 'E', 3)
+g.addEdge('C', 'D', 2)
+g.addEdge('C', 'F', 4)
+g.addEdge('D', 'E', 3)
+g.addEdge('D', 'F', 1)
+g.addEdge('E', 'F', 1)
+print(g.Dijkstra("B", "F"))
